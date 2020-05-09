e00401da619fe33da5b589f54b89ec22d6b0151f
AndriiMalchevskyi <asmalchevskyi@gmail.com>
2020-05-09 14:49:52 +0300

    Add project files.

---
+++ Naive_Bayes/Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naive_Bayes
+{
+    public class Document
+    {
+        public Document(string @class, string text)
+        {
+            Class = @class;
+            Text = text;
+        }
+        public string Class { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Naive_Bayes/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Naive_Bayes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (true)
+            {
+                Console.WriteLine("/\\/\\/\\/\\/ NAIVE BAYES \\/\\/\\/\\/\\");
+                Console.WriteLine(@"Read base base_en.txt");
+                string[] stringToParse = null;
+                try
+                {
+                    stringToParse = File.ReadAllLines(@"base_en.txt");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                if (stringToParse != null)
+                {
+                    var trainCorpus = ParseTrainCorpus(stringToParse);
+                    var bayes = new Classifier(trainCorpus);
+                    var text = "";
+
+                    Console.Write("\nInput text to check: ");
+                    text = Console.ReadLine().ToLower();
+
+                    while (text != "exit")
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("*********RESULT*********");
+                        var resSpam = bayes.IsInClassProbability("spam", text);
+                        var resHam = bayes.IsInClassProbability("ham", text);
+                        Console.WriteLine("result\nspam: {0}\nham: {1}", resSpam, resHam);
+
+                        if (resSpam > resHam)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("I suppose it is spam");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Maybe you received message from your best friend");
+                        }
+
+                        Console.ResetColor();
+                        Console.Write("\nInput text to check: ");
+                        text = Console.ReadLine().ToLower();
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+
+        static List<Document> ParseTrainCorpus(string[] textToParse)
+        {
+            var resultList = new List<Document>();
+            foreach(var line in textToParse)
+            {
+                var trimPos = line.IndexOf(";");
+                var @class = line.Substring(0, trimPos).ToLower();
+                var text = line.Substring(trimPos+1, line.Length-trimPos-1).ToLower();
+                resultList.Add(new Document(@class, text));
+            }
+
+            return resultList;
+        }
+    }
+}
